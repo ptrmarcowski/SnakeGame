f172488d16658919e7c1cdda7b6928a1c5bab282
Piotr Marcowski <piotr.marcowski@gmail.com>
2022-01-29 18:11:29 +0100

    Added KeyAdapter, Score, Game Over text, andom color for the snake

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -47,23 +47,32 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
 
         if (running) {
+            /*
             for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }
+            */
             g.setColor(Color.red);
             g.fillOval(pointX, pointY, UNIT_SIZE, UNIT_SIZE);
 
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
                     g.setColor(Color.green);
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
                 else {
                     g.setColor(new Color(45, 180, 0));
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
                 }
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             }
+            g.setColor(Color.red);
+            g.setFont(new Font("Monospaced", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+pointsEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+pointsEaten))/2, g.getFont().getSize());
+        }
+        else {
+            gameOver(g);
         }
     }
 
@@ -120,7 +129,17 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g) {
+        //display points
+        g.setColor(Color.red);
+        g.setFont(new Font("Monospaced", Font.BOLD, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+pointsEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+pointsEaten))/2, g.getFont().getSize());
 
+        //game over text
+        g.setColor(Color.red);
+        g.setFont(new Font("Monospaced", Font.BOLD, 75));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over!", (SCREEN_WIDTH - metrics2.stringWidth("Game Over!"))/2, SCREEN_HEIGHT/2);
     }
 
     public void actionPerformed(ActionEvent actionEvent) {
